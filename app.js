9c25e39fceab0245597e42c197b4e2e7cf98a878
Annel Angulo Calleros <info.annelanguloc@gmail.com>
2025-03-14 14:05:47 -0600

    Update app.js
    Se creó una función para agregar nombres de amigos a la lista.

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //-------Crear lista----------
 let amigo = [];
+//-------Función para agregar nombres de amigos ----------------------
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
+    // !-- si no se ingresa nombre de amigo 
+    if (!nombreAmigo) {
+        alert ("Debe ingresar un nombre");
+        return;
+    }
+    amigo.push(nombreAmigo);
+    inputAmigo.value = "";
+    inputAmigo.focus();
+    cargaAmigos();
+    
+}
